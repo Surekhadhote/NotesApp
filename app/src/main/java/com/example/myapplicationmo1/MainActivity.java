00cb13f3b14405e7afe4e670907b4d712ba3e9ec
aarti <aarti6264744312@gmail.com>
2020-08-04 13:26:22 +0530

    Project Setup Done

---
+++ app/src/main/java/com/example/myapplicationmo1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationmo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
