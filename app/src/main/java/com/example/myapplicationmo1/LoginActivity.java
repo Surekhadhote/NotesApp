2132c1c07c0d3043fd96c0222bd5aabc514c6daf
aarti <aarti6264744312@gmail.com>
2020-08-15 10:36:55 +0530

    pop up activity done

--- app/src/main/java/com/example/myapplicationmo1/LoginActivity.java
+++ app/src/main/java/com/example/myapplicationmo1/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplicationmo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -11,10 +12,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import static com.example.myapplicationmo1.PrefConstant.IS_LOGGED_IN;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText editTextFullName,editTextUserName;
     Button buttonLogin;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
         editTextFullName = findViewById(R.id.editTextFullName);
         editTextUserName = findViewById(R.id.editTextUserName);
         buttonLogin = findViewById(R.id.buttonLogin);
+        setupSharedPreferences();
+
        View.OnClickListener clickAction = new View.OnClickListener(){
           @Override
           public void onClick(View view){
@@ -34,13 +41,32 @@ public class LoginActivity extends AppCompatActivity {
               if(!TextUtils.isEmpty(fullName) && !TextUtils.isEmpty(UserName)) {
 
                   Intent intent = new Intent(LoginActivity.this, MyNotesActivity.class);
-                  intent.putExtra("full_name", fullName);
+                  intent.putExtra(AppConstant.Full_Name, fullName);
                   startActivity(intent);
+
+                  saveLoginStatus();
+                  saveFullName(fullName);
               } else{
-                  Toast.makeText(LoginActivity.this,"FullName and UserName are mendatory",Toast.LENGTH_SHORT).show();
+                  Toast.makeText(LoginActivity.this,"FullName and UserName both are mendatory",Toast.LENGTH_SHORT).show();
               }
           }
        };
            buttonLogin.setOnClickListener(clickAction);
+    }
+
+    private void saveFullName(String fullName) {
+        editor = sharedPreferences.edit();
+        editor.putString(PrefConstant.FULL_NAME,fullName);
+        editor.apply();
+    }
+
+    private void saveLoginStatus() {
+        editor = sharedPreferences.edit();
+        editor.putBoolean(IS_LOGGED_IN,true);
+        editor.apply();
+    }
+
+    private void setupSharedPreferences() {
+        sharedPreferences = getSharedPreferences(PrefConstant.SHARED_PREFERENCE_NAME,MODE_PRIVATE);
     }
 }
---
+++ app/src/main/java/com/example/myapplicationmo1/clickListener/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.myapplicationmo1.clickListener;
+
+import com.example.myapplicationmo1.Model.Notes;
+
+public interface ItemClickListener {
+    void onClick(Notes notes);
+}
